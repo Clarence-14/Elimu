c3a7d396f4ee19c6c54183ce1025e0045d5e0144
Clarence-14 <thisisclarence1@gmail.com>
2024-06-24 10:44:52 +0300

    first commit

---
+++ elimuapp/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from elimuapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('gallery/', views.gallery),
+    path('info/', views.information),
+    path('table/', views.table)
+
+]
